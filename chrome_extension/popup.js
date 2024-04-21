79ed51ef7bf7354f13fd7e153e6f3c4369c00336
Peter <petersweeney1028@gmail.com>
2024-04-21 17:16:00 -0400

    updated the apis in the popup

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,9 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('savePageButton').addEventListener('click', function() {
+    const button = document.getElementById('savePageButton');
+    button.addEventListener('click', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            var activeTab = tabs[0];
-            chrome.storage.sync.set({[activeTab.url]: new Date().toISOString()}, function() {
+            const activeTab = tabs[0];
+            fetch('https://read-later-summary-e391515c4221.herokuapp.com/api/save_article', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    url: activeTab.url,
+                    title: activeTab.title
+                })
+            })
+            .then(response => {
+                if(response.ok) {
+                    return response.json();
+                }
+                throw new Error('Something went wrong with the server!');
+            })
+            .then(data => {
+                console.log('Success:', data);
                 document.getElementById('status').innerText = 'Page saved!';
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                document.getElementById('status').innerText = 'Save failed!';
             });
         });
     });
